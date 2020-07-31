0360ed4872d8dbab07bebd854f9c579b5267cfd9
Jan Janki Uskoba <jan.uskoba@hyperia.sk>
2020-07-31 14:59:04 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
-# yii2-clearCacheBehavior
-The behavior for Yii2 to clearing cache on specific events
+# Yii2 clear cache behavior
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/hyperia-sk/yii2-clear-cache-behavior/master/LICENSE) 
+> The behavior for Yii2 to clearing cache on specific events
+
+## Instalation
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```shell
+composer require hyperia/yii2-clear-cache-behavior:"*"
+```
+
+or add
+
+```
+"hyperia/yii2-clear-cache-behavior": "*"
+```
+
+to the require section of your composer.json.
+
+## Configuration (usage)
+In ActiveRecord Model class to invalidate tag after insert, update or delete
+```php
+
+use yii\db\ActiveRecord;
+use hyperia\behaviors\ClearCacheBehavior;
+
+class Model extends ActiveRecord
+{
+    const CACHE_KEY = '~model~';
+
+    public function behaviors()
+    {
+        return [
+            ...
+            'clearCache' => [
+                'class' => ClearCacheBehavior::class,
+                'events' => [
+                    ActiveRecord::EVENT_AFTER_INSERT,
+                    ActiveRecord::EVENT_AFTER_UPDATE,
+                    ActiveRecord::EVENT_AFTER_DELETE
+                ],
+                'type' => ClearCacheBehavior::TYPE_INVALIDATE_TAG,
+                'value' => static::CACHE_KEY
+            ],
+        ];
+    }
+}
+```
+
+## Parameter description
+
+### events
+*array*
+Determinantes on which event would be cache deleted. When you want set up Event with same settings.
+
+Default value:
+[
+    ActiveRecord::EVENT_AFTER_INSERT,
+    ActiveRecord::EVENT_AFTER_UPDATE,
+    ActiveRecord::EVENT_AFTER_DELETE
+]
+
+### cache
+*string*
+Name of cache component in yii components configuration
+**Default: "cache"**
+
+### value 
+*string | array | Closure*
+Determinantes which part of cache would be deleted **ONLY WHEN EVENTS IS SET**
+
+
+### type
+*string*
+Sets how the cache will be deleted **ONLY WHEN EVENTS IS SET**
+Types:
+ - TYPE_INVALIDATE_TAG : Calls yii\caching\TagDependency::invalidate($cacheObject, $value);
+ - TYPE_FLUSH : Calls flush() method on cache object (by value of cache parameter)
+ - TYPE_DELETE : Calls delete($value) method on cache object (by value of cache parameter)
+
+### events_with_settings
+*array*
+Array which represents setting of multiple events. Determinantes on which event would be cache deleted. When you want set up multiple Events
+Simple example:
+```php
+    'events_with_settings' => [
+          \yii\web\Controller::EVENT_BEFORE_ACTION => [
+              'type' => ClearCacheBehavior::TYPE_INVALIDATE_TAG,
+              'value' => static::CACHE_KEY
+          ],
+          \yii\web\Controller::EVENT_AFTER_ACTION => [
+              'type' => ClearCacheBehavior::TYPE_DELETE,
+              'value' => function($event) use ($model) {
+                  return $model->id;
+              }
+          ]
+    ],
+```
---
+++ src/ClearCacheBehavior.php
@@ -0,0 +1,187 @@
+<?php
+namespace hyperia\behaviors;
+
+use Yii;
+use Closure;
+use yii\base\Event;
+use yii\base\Behavior;
+use yii\caching\TagDependency;
+use yii\db\ActiveRecord;
+
+class ClearCacheBehavior extends Behavior
+{
+    const TYPE_INVALIDATE_TAG = 'invalidate';
+    const TYPE_FLUSH = 'flush';
+    const TYPE_DELETE = 'delete';
+    
+    private static $DEFAULT_FUNCTION = 'process';
+    
+    /**
+     * Array which represents setting of multiple events
+     * Determinantes on which event would be cache deleted.
+     * When you want set up multiple Events
+     * <br>
+     * Simple example:
+     * <pre><code>
+     * 'events_with_settings' => [
+     *      \yii\web\Controller::EVENT_BEFORE_ACTION => [
+     *          'type' => ClearCacheBehavior::TYPE_INVALIDATE_TAG,
+     *          'value' => static::CACHE_KEY
+     *      ],
+     *      \yii\web\Controller::EVENT_AFTER_ACTION => [
+     *          'type' => ClearCacheBehavior::TYPE_DELETE,
+     *          'value' => function($event) use ($model) {
+     *              return $model->id;
+     *          }
+     *      ]
+     * ],
+     * </pre></code>
+     * @var array
+     */
+    public $events_with_settings;
+    
+    /**
+     * Determinantes on which event would be cache deleted.
+     * When you want set up Event with same settings
+     *
+     * @var array
+     */
+    public $events = [
+        ActiveRecord::EVENT_AFTER_INSERT,
+        ActiveRecord::EVENT_AFTER_UPDATE,
+        ActiveRecord::EVENT_AFTER_DELETE
+    ];
+    
+    
+    public $only;
+    
+    /**
+     * Sets how the cache will be deleted
+     * ONLY WHEN EVENTS IS SET
+     *
+     * @var string
+     */
+    public $type;
+    
+    /**
+     * Determinantes which part of cache would be deleted
+     * ONLY WHEN EVENTS IS SET
+     *
+     * @var string | array | Closure
+     */
+    public $value;
+    
+    /**
+     * Name of cache component in yii components configuration
+     *
+     * @var string
+     */
+    public $cache = 'cache';
+    
+    /**
+     * Cache object component
+     *
+     * @var \yii\base\Component
+     */
+    private $cacheObject;
+    
+    
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        
+        try {
+            $this->cacheObject = Yii::$app->get($this->cache);
+        } catch (\yii\base\Exception $ex) {
+            throw new \yii\base\InvalidArgumentException("Component $this->cache not found " . $ex->getMessage());
+        }
+    }
+    
+    /**
+     * Clear cache on current event in the selected way
+     * @param Event $event
+     */
+    public function process(Event $event)
+    {
+        if (!empty($this->only) && isset($event->action) && !in_array($event->action->id, $this->only)) {
+            return;
+        }
+        
+        if (!empty($this->events_with_settings) && array_key_exists($event->name, $this->events_with_settings)) {
+            $type = $this->events_with_settings[$event->name]['type'];
+            $value = $this->getValue($event, $this->events_with_settings[$event->name]['value']);
+            $this->$type($event, $value);
+        }
+        
+        if (!empty($this->events) && in_array($event->name, $this->events)) {
+            $type = $this->type;
+            $this->$type($event, $this->getValue($event, $this->value));
+        }
+    }
+    
+    /**
+     * Invalidates all of the cached data items that are associated with any of the specified [[tags]].
+     * @param Event $event
+     * @param mixed $value
+     */
+    public function invalidate(Event $event, $value)
+    {
+        TagDependency::invalidate($this->cacheObject, $value);
+    }
+    
+    /**
+     * Deletes a value with the specified key from cache
+     * @param Event $event
+     * @param type $value
+     */
+    public function delete(Event $event, $value)
+    {
+        $this->cacheObject->delete($value);
+    }
+    
+    /**
+     * Deletes all values from cache.
+     * @param Event $event
+     * @param type $value
+     */
+    public function flush(Event $event, $value)
+    {
+        $this->cacheObject->flush();
+    }
+
+    /**
+     * @PHPUnitGen\AssertNotEmpty()
+     * @PHPUnitGen\AssertInternalType('array')
+     * @return array
+     */
+    public function events()
+    {
+        return [
+            \yii\web\Controller::EVENT_AFTER_ACTION => static::$DEFAULT_FUNCTION,
+            \yii\web\Controller::EVENT_BEFORE_ACTION => static::$DEFAULT_FUNCTION,
+            \yii\db\BaseActiveRecord::EVENT_AFTER_INSERT => static::$DEFAULT_FUNCTION,
+            \yii\db\BaseActiveRecord::EVENT_AFTER_DELETE => static::$DEFAULT_FUNCTION,
+            \yii\db\BaseActiveRecord::EVENT_AFTER_UPDATE => static::$DEFAULT_FUNCTION,
+            \yii\db\BaseActiveRecord::EVENT_AFTER_REFRESH => static::$DEFAULT_FUNCTION
+        ];
+    }
+    
+    /**
+     * Returns the value for the current type of deletion.
+     *
+     * @PHPUnitGen\AssertNotEmpty(:{null, 'foo'})
+     * @param Event $event the event that triggers delete of cache.
+     * @return mixed the attribute value
+     */
+    protected function getValue($event, $value)
+    {
+        if ($value instanceof Closure || (is_array($value) && is_callable($value))) {
+            return call_user_func($value, $event);
+        }
+        
+        return $value;
+    }
+}
